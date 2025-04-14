f3886fe66f885b6e87d98607852440498d1da5e2
Kathleen Fillingim <kpfillingim@yahoo.com>
2025-04-13 23:37:59 -0400

    * Adjusted average() function to properly calculate average of 3 numbers by adding parentheses. * Changed a duplicate instance of variable 'n2' into 'n3' to properly store user input into 3 variables, not 2. * Adjusted formatting of code & console output for user convenience.

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -4,20 +4,27 @@ using namespace std;
 
 float average(int i1, int i2, int i3)
 {
-	return i1 + i2 + i3 / 3.0f;
+	return ((i1 + i2 + i3) / 3.0);
 }
 
 int main()
 {
-	int n1 =0 , n2 = 0, n3 = 0;
-	cout << "This program calculates the average of three numbers." << endl;
-	cout << "First number: "; cin >> n1;
-	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+	int n1 = 0, n2 = 0, n3 = 0;
+
+	cout << "\nThis program calculates the average of three numbers." << endl;
+	cout << "\nEnter the first number: "; 
+	cin >> n1;
+
+	cout << "Enter the second number: ";
+	cin >> n2;
+
+	cout << "Enter the third number: ";
+	cin >> n3;
 
 	float a = average(n1, n2, n3);
 
-	cout << setprecision(1) << fixed << "The average is " << a << endl;
+	cout << setprecision(1) << fixed;
+	cout << "\nThe average is " << a << endl;
 
 	return 0;
 }
